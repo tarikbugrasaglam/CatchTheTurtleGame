2444b93e3ba4ef7a9d62e034245937366f75624a
Tarık Buğra Sağlam <trakal55@hotmail.com>
2024-10-15 20:39:45 +0300

    Süre bittiğinde scor artık tıklandığında artmıyor.

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 import turtle
 import time
+import random
 from random import randint
-from idlelib.configdialog import font_sample_text
 
 game_screen=turtle.Screen()
 game_screen.bgcolor("white")
@@ -9,36 +9,41 @@ game_screen.title("Catch The Turtle!")
 
 game_screen.tracer(0)
 
+#ekrandaki turtle'ın kodları
 ken=turtle.Turtle()
 ken.shape("turtle")
 ken.color("dark green")
 ken.shapesize(2,2)
 
+#Turtle timer kodları
 timer_text=turtle.Turtle()
-timer_text.hideturtle()  # Hide the turtle icon
-timer_text.penup()       # Stop drawing the turtle trail
+timer_text.hideturtle()
+timer_text.penup()
 
+#Turtle score kodları
 score=0
 score_text=turtle.Turtle()
 score_text.hideturtle()
 score_text.penup()
 score_text.goto(0,300)
 
+game_over=False #oyun bittiğinde daha fazla tıkladığımızda puan almamak için.
+
+#score'u kontrol eden fonksiyon
 def increase_score(x,y):
     global score
-    if ken.distance(x,y)<20: #kaplumbağa mesafe kontrolü
+    if not game_over and ken.distance(x, y) < 20:
         score+=1
         score_text.clear()
         score_text.write(f"Score: {score}",align="center",font=("Courier", 30, "normal"))
 
-game_screen.onscreenclick(increase_score)
-
-
 start=time.time()
 previous_time=-1 #önceki zamanı tutan değişken
 
-while (time.time()-start)<16:
-    current_time=int(time.time()-start)
+while int(time.time()-start)<16:
+
+    #Zamanı hesaplama
+    current_time = int(time.time() - start)
 
     if current_time!=previous_time:
         timer_text.clear()
@@ -48,13 +53,11 @@ while (time.time()-start)<16:
         ken.pendown()
         timer_text.write(f"Time={current_time}", font=("Courier", 30, "normal"))
         previous_time = current_time
+        game_screen.onscreenclick(increase_score)
 
     game_screen.update()
-    #time.sleep(0.100)  # Kaplumbağanın hızını yavaşlatmak için bekleme süresi
 
-score_text.goto(0, 0)  # Sonucu ortala
-score_text.write(f"Times is done\n Score: {score}", align="center", font=("Courier", 30, "normal"))
-
-
-
+game_over = True
+score_text.goto(0, 0)
+score_text.write(f"Times is finished\n Score: {score}", align="center", font=("Courier", 30, "normal"))
 turtle.mainloop()
